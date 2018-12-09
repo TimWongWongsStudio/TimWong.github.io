aeb691bf2eb666ea92dd86752772527b500b18c9
TimWongWongsStudio <45726610+TimWongWongsStudio@users.noreply.github.com>
2018-12-09 13:14:02 +0800

    Website upload

---
+++ whatsnews.php
@@ -0,0 +1,10 @@
+<marquee behavior="scroll" direction="up" scrollamount="1" height="170" width="165">
+<p><span style="color:#FF0000">●</span> <a href="http://mysteryshopper.cc/smiling2018.pdf" target="_blank">2017/18年度「微笑企業」大獎現已接受報名</a></p>
+<br />
+<p><span style="color:#FF0000">●</span> <a href="http://mysteryshopper.cc/smiling2016.pdf" target="_blank">2016/17年度「微笑企業」大獎現已接受報名</a></p>
+<br />
+<p><span style="color:#FF0000">●</span> <a href="http://mysteryshopper.cc/ch/sr_2016.php" target="_blank">2016 微笑報告</a></p>
+<br />
+<p><span style="color:#FF0000">●</span> <a href="http://fb.com/mysteryshoppers" target="_blank">Facebook 關注我們的動態</a></p>
+</marquee>
+
